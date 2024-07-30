98106ba8a834ecfbdb0c408ad78ff92d8a740ce9
Vinicius Mattoso <vinicius.vmrs@gmail.com>
2024-07-30 10:33:35 -0300

    creating the possibility to run the slurm_template_with_mlflow

--- app/controllers/slurm/slurm_manager.py
+++ app/controllers/slurm/slurm_manager.py
@@ -13,7 +13,10 @@ def read_template(template_path):
 def prepare_srm_template(task_dict):
     """Prepare srm template with task_dict"""
     if "atena" in task_dict['runner_location']:
-        template = read_template("app/controllers/slurm/slurm_template.srm")
+        if 'mlflow' in task_dict['execution_mode']:
+            template = read_template("app/controllers/slurm/slurm_template_with_mlflow.srm")
+        else:
+            template = read_template("app/controllers/slurm/slurm_template.srm")
     elif "dev" in task_dict['runner_location']:
         template = read_template("app/controllers/slurm/dev_template.srm")
     slurm_script = template.format(
@@ -27,6 +30,7 @@ def prepare_srm_template(task_dict):
         sif_path = settings.sif_root,
         atena_root = settings.atena_root,
         nfs_root = settings.nfs_root,
+        project_root=task_dict['project_path'],
     )
     fname = f'slurm_script_{task_dict["id"]}.srm'
     fpath = save_file(fname, slurm_script,task_dict["id"])
